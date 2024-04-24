7bb8dcb9aff18ce985650a8019de578ca0285e51
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-04-24 11:35:49 +0600

    create payment intent

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require("cors");
+const stripe = require("stripe")(process.env.PAYMENT_SECRET);
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId, ClientSession } = require('mongodb');
 const port = 3000
@@ -164,6 +165,29 @@ async function run() {
       const result = await cartCollection.deleteOne(query);
       res.send(result);
     })
+
+
+    // ---------------------------PAYMENTS ROUTES -----------------------------
+
+
+    // create payment intent
+
+    app.post("/create-payment-intent", async (req, res) => {
+      const {price} = req.body;
+      const amount = parseInt(price) * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount:amount,
+        currency: "usd",
+        payment_method_types : ['card']
+      })
+
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+
+
+    })
+
 
 
 
