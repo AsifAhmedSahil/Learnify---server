76e443dc7254121976f9ba0f1942e407ad93e5cb
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-04-30 11:26:18 +0600

    delere user

--- index.js
+++ index.js
@@ -70,6 +70,14 @@ async function run() {
       res.send(result)
     })
 
+    // delete user
+    app.delete("/user/:id",async(req,res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await usersCollection.deleteOne(query)
+      res.send(result)
+    })
+
 
     // Setup classes routes here
     app.post("/new-class", async (req, res) => {
